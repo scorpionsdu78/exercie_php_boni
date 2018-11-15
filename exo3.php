eb495057e0787f8ca6914669dac10ec16cca2a01
UbuntuDesktop <fboni.78@gmail.com>
2018-11-15 01:57:16 -0800

    knew commit

--- exo3.php
+++ exo3.php
@@ -1,13 +1,19 @@
 <head>
 </head>
 
-<body style="text-align: center; font-size: 50px;">
+<body style="font-size: 50px;">
 <br>
 
-<form action="exo2.php" method="get">
+<form action="exo3.php" method="get">
 name: <input type="text" name="name">
 <input type="submit">
 </form>
+
+<form action="exo3.php" method="post">
+filter: <input type="text" name="filter">
+<input type="submit">
+</form>
+
 
 <?php
 	
@@ -17,19 +23,57 @@ name: <input type="text" name="name">
 	$name = "world";
 	
 	if(isset($_GET['name'])){
-		
+		?> <ul style="margin-left:40%;" > <?php
 		$name = $_GET['name'];
-		$name = "$name \n";
-		file_put_contents($file, $name,FILE_APPEND);
+		if($name != "blank")
+		{
+			$name = "$name \n";
+			file_put_contents($file, $name,FILE_APPEND);
+		
 	
+			$file = fopen("ami.txt","r");
+		
+			while(!feof($file))	
+			{
+				?>
+					<li><?php echo fgets($file); ?></li>
+			
+				<?php
+			}
+			fclose($file);
+		}
+		
+		
+		?> </ul> <?php
+	}
+	?>
+	
+	<?php
+	if(isset($_POST['filter'])){
+		?> <ul style="margin-left:40%;" > <?php
+		$filter = $_POST['filter'];
+	
+		
+	
+
 		$file = fopen("ami.txt","r");
-	
+
 		while(!feof($file))	
 		{
-			echo fgets($file)."<br>";
+			$line = fgets($file);
 			
+			if(strstr($line,$filter))
+			{
+				?>
+					<li><?php echo $line; ?></li>
+			
+				<?php
+			}
 		}
 		fclose($file);
+	
+		
+		?> </ul> <?php
 		
 	}
 	
